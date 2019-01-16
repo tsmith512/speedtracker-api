9a5ad20ac088061f92bdf2fbc1030bbddff433e4
Taylor Smith <tsmith@tsmithcreative.com>
2019-01-16 20:04:41 +0000

    Make this executable by command line

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const config = require('./config')
 const ErrorHandler = require('./lib/ErrorHandler')
 const GitHub = require('./lib/GitHub')
 const SpeedTracker = require('./lib/SpeedTracker')
+const argv = require('yargs').argv;
 
 
 // ------------------------------------
@@ -14,65 +15,49 @@ const github = new GitHub()
 
 github.authenticate(config.get('githubToken'))
 
-// ------------------------------------
-// DB connection
-// @TODO: No Dataabse or Scheduler init here, which only leaves
-// opening the Express server to listen on the config port, which was originally
-// in the database connection callback.
-// ------------------------------------
-
-server.listen(config.get('port'), () => {
-  console.log(`(*) Server listening on port ${config.get('port')}`)
-})
 
 // ------------------------------------
 // Endpoint: Test
 // ------------------------------------
 
-const testHandler = (req, res) => {
+const testHandler = (options) => {
   const blockList = config.get('blockList').split(',')
 
   // Abort if user is blocked
-  if (blockList.indexOf(req.params.user) !== -1) {
-    ErrorHandler.log(`Request blocked for user ${req.params.user}`)
+  if (blockList.indexOf(options.user) !== -1) {
+    ErrorHandler.log(`Request blocked for user ${options.user}`)
 
     return res.status(429).send()
   }
 
   const speedtracker = new SpeedTracker({
-    branch: req.params.branch,
-    key: req.query.key,
+    branch: options.branch,
     remote: github,
-    repo: req.params.repo,
-    user: req.params.user
+    repo: options.repo,
+    user: options.user
   })
 
-  let profileName = req.params.profile
+  let profileName = options.profile
 
   speedtracker.runTest(profileName).then(response => {
-    res.send(JSON.stringify(response))
+    console.log(JSON.stringify(response))
   }).catch(err => {
     ErrorHandler.log(err)
-
-    res.status(500).send(JSON.stringify(err))
+    console.log(JSON.stringify(err))
   })
 }
 
-server.get('/v1/test/:user/:repo/:branch/:profile', testHandler)
-server.post('/v1/test/:user/:repo/:branch/:profile', testHandler)
+// server.get('/v1/test/:user/:repo/:branch/:profile', testHandler)
+// server.post('/v1/test/:user/:repo/:branch/:profile', testHandler)
 
-// ------------------------------------
-// Endpoint: Catch all
-// ------------------------------------
+const testOptions = {
+  user: argv.user,
+  repo: argv.repo,
+  branch: argv.branch,
+  profile: argv.profile
+};
 
-server.all('*', (req, res) => {
-  const response = {
-    success: false,
-    error: 'INVALID_URL_OR_METHOD'
-  }
-
-  res.status(404).send(JSON.stringify(response))
-})
+testHandler(testOptions);
 
 // ------------------------------------
 // Basic error logging
